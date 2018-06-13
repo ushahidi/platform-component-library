d0a70da9ff7eaee6cda0224ff34d147e3de92e28
Anna <anna@ushahidi.com>
2018-06-13 15:10:26 +0200

    adding button-component

--- src/components/atoms/button/index.js
+++ src/components/atoms/button/index.js
@@ -1,62 +1,46 @@
 /* @flow */
 import * as React from 'react'
 import classnames from 'classnames'
-
-import styles from './style.css'
+import button from './button.scss';
 
 export const ButtonType = {
   BUTTON: 'button',
-  RESET: 'reset',
-  SUBMIT: 'submit',
-}
-
-export const ButtonTheme = {
-  DEFAULT: 'default',
-  ROUNDED: 'rounded',
-}
-
-export const ButtonSize = {
-  SMALL: 'small',
-  MEDIUM: 'medium',
-  LARGE: 'large',
-}
+  BETA: 'button-beta',
+  GAMMA: 'button-gamma',
+  DISTRUCTIVE: 'button-destructive'
+};
 
 type Props = {
   type: string,
-  theme: string,
-  size: string,
   onClick: Function,
   children: React.Node,
   className: string,
   disabled: boolean,
-}
+};
 
 const Button = (props: Props): React.Element<*> => {
-  const { type, onClick, children, theme, size, className, disabled } = props
-  const classProps: string = classnames(
-    styles.button,
-    styles[theme],
-    styles[size],
+  const { type, onClick, children, className, disabled } = props;
+  const classProps = classnames(
+    button.button,
+    button[ButtonType[props.type]],
     {
-      [styles.disabled]: disabled,
+      [button.disabled]: disabled,
     },
-    className
-  )
-
+    button[className],
+    button['button']
+);
   return (
-    <button type={type} onClick={onClick} disabled={disabled} className={classProps}>
+    <button type="button" onClick={onClick} disabled={disabled} className={classProps}>
       {children}
     </button>
-  )
+);
 }
 
 Button.defaultProps = {
   type: ButtonType.BUTTON,
-  theme: ButtonTheme.DEFAULT,
-  size: ButtonSize.MEDIUM,
   onClick: () => {},
   className: '',
   disabled: false,
-}
+};
 
-export default Button
+export default Button;
