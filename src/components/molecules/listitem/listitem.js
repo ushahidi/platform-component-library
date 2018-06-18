41751b9f9122dbf5fc6139624b8dfef3d4343599
Anna <anna@ushahidi.com>
2018-06-18 21:47:08 +0200

    Adding list-item

---
+++ src/components/molecules/listitem/listitem.js
@@ -0,0 +1,34 @@
+/* @flow */
+import * as React from 'react'
+import classnames from 'classnames'
+import listitem from './listitem.scss';
+
+
+type Props = {
+  className: string,
+  label: string,
+  id:string,
+  children: React.Node
+};
+
+const ListItem = (props: Props): React.Element<*> => {
+  const { className, label, children, id } = props;
+  const classProps = classnames(
+    listitem['list-item'],
+    listitem[className],
+);
+console.log(listitem)
+  return (
+    <li className={classProps}>
+        {children}
+    </li>
+);
+}
+
+ListItem.defaultProps = {
+    className: '',
+    label:'',
+    id:''
+};
+
+export default ListItem;
